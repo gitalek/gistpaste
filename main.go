8bc9f2067e29a720c40c7b7d88d824c01b624686
Alexander <shalek.edu@mail.ru>
2021-02-13 14:58:11 +1000

    restrict root url pattern

--- main.go
+++ main.go
@@ -9,6 +9,10 @@ import (
 const port = 4000
 
 func home(w http.ResponseWriter, r *http.Request)  {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from Gistpaste!"))
 }
 
