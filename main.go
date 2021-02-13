eca87cc92a6a25e34a8cb63cb74ae2ec73534ce8
Alexander <shalek.edu@mail.ru>
2021-02-13 14:29:16 +1000

    add route home

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+const port = 4000
+
+func home(w http.ResponseWriter, r *http.Request)  {
+	w.Write([]byte("Hello from Gistpaste!"))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	addr := fmt.Sprintf(":%d", port)
+	log.Printf("Starting server on :%s\n", addr)
+	log.Fatal(http.ListenAndServe(addr, mux))
 }
