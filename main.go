b05d908b6defacc86e20f7a87b9f39c5a4090dcb
Alexander <shalek.edu@mail.ru>
2021-02-13 14:37:47 +1000

    add two new routes: show and create gist

--- main.go
+++ main.go
@@ -12,9 +12,19 @@ func home(w http.ResponseWriter, r *http.Request)  {
 	w.Write([]byte("Hello from Gistpaste!"))
 }
 
+func showGist (w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Display a specific gist..."))
+}
+
+func createGist (w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Create a new gist..."))
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/gist", showGist)
+	mux.HandleFunc("/gist/create", createGist)
 
 	addr := fmt.Sprintf(":%d", port)
 	log.Printf("Starting server on :%s\n", addr)
