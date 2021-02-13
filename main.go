6e67dbf155b2f1299f809ba2ccf390d5c80a1481
Alexander <shalek.edu@mail.ru>
2021-02-13 17:27:25 +1000

    validate query param "id"

--- main.go
+++ main.go
@@ -4,11 +4,21 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 const port = 4000
 
-func home(w http.ResponseWriter, r *http.Request)  {
+type ErrBadRequest struct {
+	StatusCode int
+	message    string
+}
+
+func (e ErrBadRequest) Error() string {
+	return e.message
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
@@ -16,18 +26,47 @@ func home(w http.ResponseWriter, r *http.Request)  {
 	w.Write([]byte("Hello from Gistpaste!"))
 }
 
-func showGist (w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Display a specific gist..."))
+func validateParamId(p string) (int, *ErrBadRequest) {
+	if p == "" {
+		return 0, &ErrBadRequest{
+			StatusCode: http.StatusBadRequest,
+			message:    "id query param is not found"}
+	}
+	id, err := strconv.Atoi(p)
+	if err != nil {
+		return 0, &ErrBadRequest{
+			StatusCode: http.StatusBadRequest,
+			message:    "your id query param is not a number [should be positive integer]",
+		}
+	}
+	if id < 1 {
+		return 0, &ErrBadRequest{
+			StatusCode: http.StatusBadRequest,
+			message:    "your id query param is less than or equal to zero [should be more than or equal to one]",
+		}
+	}
+	return id, nil
 }
 
-func createGist (w http.ResponseWriter, r *http.Request) {
+func showGist(w http.ResponseWriter, r *http.Request) {
+	p := r.URL.Query().Get("id")
+	id, err := validateParamId(p)
+	if err != nil {
+		w.WriteHeader(err.StatusCode)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	fmt.Fprintf(w, "Display a specific gist with ID %d", id)
+}
+
+func createGist(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
 		statusCode := 405
 		http.Error(w, http.StatusText(statusCode), statusCode)
 		return
 	}
-    w.Write([]byte("Create a new gist..."))
+	w.Write([]byte("Create a new gist..."))
 }
 
 func main() {
