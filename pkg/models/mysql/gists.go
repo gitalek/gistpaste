308d50bcfd2b308d722aa3d9f1f65684bae7bef4
Alexander <shalek.edu@mail.ru>
2021-02-15 19:35:54 +1000

    data access level: implement Insert method (model: gists)

--- pkg/models/mysql/gists.go
+++ pkg/models/mysql/gists.go
@@ -10,7 +10,20 @@ type GistModel struct {
 }
 
 func (m GistModel) Insert(title, content, expires string) (int, error) {
-	return 0, nil
+	stmt := `INSERT INTO gists (title, content, created, expires)
+				VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+
+	result, err := m.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
 }
 
 func (m GistModel) Get(id string) (*models.Gist, error) {
