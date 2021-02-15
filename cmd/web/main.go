f1b5855a91086fbf20bc3f1bf089ee4b68d22fee
Alexander <shalek.edu@mail.ru>
2021-02-15 18:16:35 +1000

    db: establish connection pool

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"net/http"
 	"os"
@@ -15,10 +17,22 @@ type application struct {
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	// dsn is a data source name (or connection string)
+	dsn := flag.String(
+		"dsn",
+		"web:password@/gistpaste?parseTime=true",
+		"MySQL data source name",
+	)
 	flag.Parse()
 
 	infolog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
 
 	app := &application{
 		errorLog: errorLog,
@@ -32,4 +46,15 @@ func main() {
 	}
 	infolog.Printf("Starting server on %s\n", *addr)
 	errorLog.Fatal(srv.ListenAndServe())
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
