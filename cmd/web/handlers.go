4b9059b4677f5a929474586e117997cfe52a5f5e
Alexander <shalek.edu@mail.ru>
2021-02-17 00:01:02 +1000

    scale data validation

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,11 +3,10 @@ package main
 import (
 	"errors"
 	"fmt"
+	"github.com/gitalek/gistpaste/pkg/forms"
 	"github.com/gitalek/gistpaste/pkg/helpers"
 	"github.com/gitalek/gistpaste/pkg/models"
 	"net/http"
-	"strings"
-	"unicode/utf8"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -47,7 +46,9 @@ func (app *application) showGist(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) createGistForm(w http.ResponseWriter, r *http.Request)  {
-	app.render(w, r, "create.page.tmpl", nil)
+	app.render(w, r, "create.page.tmpl", &templateData{
+		Form: forms.New(nil),
+	})
 }
 
 func (app *application) createGist(w http.ResponseWriter, r *http.Request) {
@@ -56,20 +57,16 @@ func (app *application) createGist(w http.ResponseWriter, r *http.Request) {
 		app.clientError(w, http.StatusBadRequest)
 	}
 
-	title := r.PostForm.Get("title")
-	content := r.PostForm.Get("content")
-	expires := r.PostForm.Get("expires")
+	form := forms.New(r.PostForm)
+	form.Required("title", "content", "expires")
+	form.MaxLength("title", 100)
+	form.PermittedValues("expires", "365", "7", "1")
 
-	errReport := validateGistFormParams(title, content, expires)
-	if len(errReport) > 0 {
-		app.render(w, r, "create.page.tmpl", &templateData{
-			FormErrors: errReport,
-			FormData: r.PostForm,
-		})
-		return
+	if !form.Valid() {
+		app.render(w, r, "create.page.tmpl", &templateData{Form: form})
 	}
 
-	id, err := app.gists.Insert(title, content, expires)
+	id, err := app.gists.Insert(form.Get("title"), form.Get("content"), form.Get("expires"))
 	if err != nil {
 		app.serverError(w, err)
 		return
@@ -77,24 +74,4 @@ func (app *application) createGist(w http.ResponseWriter, r *http.Request) {
 
 	// Redirect user to the relevant page for newly created gist.
 	http.Redirect(w, r, fmt.Sprintf("/gist/%d", id), http.StatusSeeOther)
-}
-
-func validateGistFormParams(title string, content string, expires string) map[string]string {
-	errors := make(map[string]string)
-	if strings.TrimSpace(title) == "" {
-		errors["title"] = "can't be blank"
-	} else if utf8.RuneCountInString(title) > 100 {
-		errors["title"] = "is too long (max is 100 characters)"
-	}
-
-	if strings.TrimSpace(content) == "" {
-		errors["content"] = "can't be blank"
-	}
-
-	if strings.TrimSpace(expires) == "" {
-		errors["expires"] = "can't be blank"
-	} else if expires != "365" && expires != "7" && expires != "1" {
-		errors["expires"] = "is invalid"
-	}
-	return errors
 }
--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,17 +1,16 @@
 package main
 
 import (
+	"github.com/gitalek/gistpaste/pkg/forms"
 	"github.com/gitalek/gistpaste/pkg/models"
 	"html/template"
-	"net/url"
 	"path/filepath"
 	"time"
 )
 
 type templateData struct {
 	CurrentYear int
-	FormData    url.Values
-	FormErrors  map[string]string
+	Form        *forms.Form
 	Gist        *models.Gist
 	Gists       []*models.Gist
 }
