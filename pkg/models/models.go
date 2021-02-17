6a8d3256313d89274b14339cb11f07e367bcec85
Alexander <shalek.edu@mail.ru>
2021-02-17 21:02:26 +1000

    data access layer: add user type

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -5,12 +5,26 @@ import (
 	"time"
 )
 
-var ErrNoRecord = errors.New("models: no matching record found")
+var (
+	ErrNoRecord = errors.New("models: no matching record found")
+	// ErrInvalidCredentials is used if a user tries to login with an incorrect email or password.
+	ErrInvalidCredentials = errors.New("models: invalid credentials")
+	ErrDuplicateEmail = errors.New("models: duplicate email")
+)
 
 type Gist struct {
-	ID int
-	Title string
+	ID      int
+	Title   string
 	Content string
 	Created time.Time
 	Expires time.Time
+}
+
+type User struct {
+	ID             int
+	Name           string
+	Email          string
+	HashedPassword []byte
+	Created        time.Time
+	Active         bool
 }
