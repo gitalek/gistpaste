e9edfbd05dd957977882fd2c01ca47bb5bdb55eb
Alexander <shalek.edu@mail.ru>
2021-02-17 22:57:22 +1000

    auth: add isAuthenticated helper to check if current user is authenticated

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -49,3 +49,7 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, name stri
 
 	buf.WriteTo(w)
 }
+
+func (app *application) isAuthenticated(r *http.Request) bool {
+	return app.session.Exists(r, "authenticatedId")
+}
