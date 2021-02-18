959333aca0b1c0e8ebf8e05792d545c85f9796e7
Alexander <shalek.edu@mail.ru>
2021-02-18 20:38:03 +1000

    testing: end-to-end test showGist handler with mock dependencies

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -89,6 +90,51 @@ func TestEndToEndPing(t *testing.T) {
 			if b := string(body); b != tt.want.body {
 				t.Errorf("body: want %s; got %s", tt.want.body, b)
 			}
+		})
+	}
+}
+
+func Test_application_showGist(t *testing.T) {
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	type args struct {
+		route string
+	}
+
+	type result struct {
+		body       []byte
+		statusCode int
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want result
+	}{
+		{name: "Valid ID", args: args{route: "/gist/1"}, want: result{body: []byte("An old silent pond..."), statusCode: http.StatusOK}},
+		{name: "Non-existent ID", args: args{route: "/gist/2"}, want: result{body: nil, statusCode: http.StatusNotFound}},
+		{name: "Negative ID", args: args{route: "/gist/-1"}, want: result{body: nil, statusCode: http.StatusBadRequest}},
+		{name: "Decimal ID", args: args{route: "/gist/1.23"}, want: result{body: nil, statusCode: http.StatusBadRequest}},
+		{name: "String ID", args: args{route: "/gist/foo"}, want: result{body: nil, statusCode: http.StatusBadRequest}},
+		{name: "Empty ID", args: args{route: "/gist/"}, want: result{body: nil, statusCode: http.StatusNotFound}},
+		{name: "Trailing slash", args: args{route: "/gist/1/"}, want: result{body: nil, statusCode: http.StatusNotFound}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			statusCode, _, body := ts.get(t, tt.args.route)
+			if statusCode != tt.want.statusCode {
+				t.Errorf("status code: want %d; got %d", tt.want.statusCode, statusCode)
+			}
+			if !bytes.Contains(body, tt.want.body) {
+				t.Errorf("body: want %s; got %s", tt.want.body, body)
+			}
+			// todo: !
+			//if !bytes.Contains(tt.want.body, body) {
+			//	t.Errorf("body: want %s; got %s", tt.want.body, body)
+			//}
 		})
 	}
 }
