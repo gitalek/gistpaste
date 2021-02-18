c84b0d542d940672770aa39aec25d34ade5e8d4f
Alexander <shalek.edu@mail.ru>
2021-02-18 18:21:20 +1000

    testing: end-to-end ping handler test

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -39,6 +40,57 @@ func Test_ping(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			ping(tt.args.w, tt.args.r)
 			rs := tt.args.w.Result()
+			defer rs.Body.Close()
+			if rs.StatusCode != tt.want.statusCode {
+				t.Errorf("status code: want %d; got %d", tt.want.statusCode, rs.StatusCode)
+			}
+			body, err := ioutil.ReadAll(rs.Body)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if b := string(body); b != tt.want.body {
+				t.Errorf("body: want %s; got %s", tt.want.body, b)
+			}
+		})
+	}
+}
+
+func TestEndToEndPing(t *testing.T) {
+	app := &application{
+		errorLog: log.New(ioutil.Discard, "", 0),
+		infoLog:  log.New(ioutil.Discard, "", 0),
+	}
+
+	ts := httptest.NewTLSServer(app.routes())
+	defer ts.Close()
+
+	type args struct {
+		route string
+		ts    *httptest.Server
+	}
+
+	type result struct {
+		body       string
+		statusCode int
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want result
+	}{
+		{
+			name: "first",
+			args: args{route: "/ping", ts: ts},
+			want: result{body: "OK", statusCode: http.StatusOK},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			rs, err := ts.Client().Get(tt.args.ts.URL + tt.args.route)
+			if err != nil {
+				t.Fatal(err)
+			}
 			defer rs.Body.Close()
 			if rs.StatusCode != tt.want.statusCode {
 				t.Errorf("status code: want %d; got %d", tt.want.statusCode, rs.StatusCode)
