c075c8e95feba536a76661f7a8615f171b9dbc69
Alexander <shalek.edu@mail.ru>
2021-02-19 16:03:44 +1000

    testing: end-to-end test for the "POST /user/signup" route

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 )
 
@@ -135,6 +136,68 @@ func Test_application_showGist(t *testing.T) {
 			//if !bytes.Contains(tt.want.body, body) {
 			//	t.Errorf("body: want %s; got %s", tt.want.body, body)
 			//}
+		})
+	}
+}
+
+func Test_application_signupUser(t *testing.T) {
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	route := "/user/signup"
+	_, _, body := ts.get(t, route)
+	csrfToken := extractCSRFToken(t, body)
+
+	type formData struct {
+		userName     string
+		userEmail    string
+		userPassword string
+		csrfToken    string
+	}
+
+	type args struct {
+		formData formData
+	}
+
+	type result struct {
+		body       []byte
+		statusCode int
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want result
+	}{
+		{name: "Valid submission", args: args{formData: formData{userName: "Bob", userEmail: "bob@example.com", userPassword: "validPa$$word", csrfToken: csrfToken}}, want: result{body: nil, statusCode: http.StatusSeeOther}},
+		{name: "Empty name", args: args{formData: formData{userName: "", userEmail: "bob@example.com", userPassword: "validPa$$word", csrfToken: csrfToken}}, want: result{body: []byte("This field cannot be blank"), statusCode: http.StatusOK}},
+		{name: "Empty email", args: args{formData: formData{userName: "Bob", userEmail: "", userPassword: "validPa$$word", csrfToken: csrfToken}}, want: result{body: []byte("This field cannot be blank"), statusCode: http.StatusOK}},
+		{name: "Empty password", args: args{formData: formData{userName: "Bob", userEmail: "bob@example.com", userPassword: "", csrfToken: csrfToken}}, want: result{body: []byte("This field cannot be blank"), statusCode: http.StatusOK}},
+		{name: "Invalid email (incomplete domain)", args: args{formData: formData{userName: "Bob", userEmail: "bob@example.", userPassword: "validPa$$word", csrfToken: csrfToken}}, want: result{body: []byte("This field is invalid"), statusCode: http.StatusOK}},
+		{name: "Invalid email (missing @)", args: args{formData: formData{userName: "Bob", userEmail: "bobexample.com", userPassword: "validPa$$word", csrfToken: csrfToken}}, want: result{body: []byte("This field is invalid"), statusCode: http.StatusOK}},
+		{name: "Invalid email (missing local part)", args: args{formData: formData{userName: "Bob", userEmail: "@example.com", userPassword: "validPa$$word", csrfToken: csrfToken}}, want: result{body: []byte("This field is invalid"), statusCode: http.StatusOK}},
+		{name: "Short password", args: args{formData: formData{userName: "Bob", userEmail: "bob@example.com", userPassword: "pa$$word", csrfToken: csrfToken}}, want: result{body: []byte("This field is too short (minimum is 10 characters)"), statusCode: http.StatusOK}},
+		{name: "Duplicate email", args: args{formData: formData{userName: "Bob", userEmail: "error@example.com", userPassword: "validPa$$word", csrfToken: csrfToken}}, want: result{body: []byte("The address is already in use"), statusCode: http.StatusOK}},
+		{name: "Invalid CSRF Token", args: args{formData: formData{userName: "", userEmail: "", userPassword: "", csrfToken: "wrongToken"}}, want: result{body: nil, statusCode: http.StatusBadRequest}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			form := url.Values{}
+			form.Add("name", tt.args.formData.userName)
+			form.Add("email", tt.args.formData.userEmail)
+			form.Add("password", tt.args.formData.userPassword)
+			form.Add("csrf_token", tt.args.formData.csrfToken)
+
+			statusCode, _, body := ts.postForm(t, route, form)
+
+			if statusCode != tt.want.statusCode {
+				t.Errorf("status code: want %d; got %d", tt.want.statusCode, statusCode)
+			}
+			if !bytes.Contains(body, tt.want.body) {
+				t.Errorf("body: want %s; got %s", tt.want.body, body)
+			}
 		})
 	}
 }
